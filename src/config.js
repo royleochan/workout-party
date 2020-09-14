f363cf923664d2dfa952d4ab660670b339a17a19
royleochan <royleochan@gmail.com>
2020-09-14 19:01:07 +0800

    Setting up aws config

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-1",
+    BUCKET: "workout-app-file-uploads"
+  },
+  // TODO
+  // apiGateway: {
+  //   REGION: "YOUR_API_GATEWAY_REGION",
+  //   URL: "YOUR_API_GATEWAY_URL"
+  // },
+  cognito: {
+    REGION: "ap-southeast-1",
+    USER_POOL_ID: "ap-southeast-1_tVQKfWCtt",
+    APP_CLIENT_ID: "31gfr41ftu1kd9t8lhdpp5u5q8",
+    // TODO
+    // IDENTITY_POOL_ID: "ap-southeast-1_tVQKfWCtt"
+  }
+};
